1119df6a6115a1c5d2b86d62b6f8bbdb69d85929
Saliya Ekanayake <esaliya@gmail.com>
2015-11-07 02:44:46 -0500

    Removing unused import

--- src/main/java/org/saliya/javathreads/BusySqrt.java
+++ src/main/java/org/saliya/javathreads/BusySqrt.java
@@ -1,7 +1,10 @@
 package org.saliya.javathreads;
 
+import net.openhft.affinity.Affinity;
+
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.BitSet;
 import java.util.concurrent.CountDownLatch;
 
 public class BusySqrt implements Runnable{
@@ -9,17 +12,23 @@ public class BusySqrt implements Runnable{
     private final int threadIdx;
     private final boolean bind;
     private final CountDownLatch latch;
+    private final int cores;
 
     public BusySqrt(
-        double[] results, int threadIdx, boolean bind, CountDownLatch latch) {
+        double[] results, int threadIdx, boolean bind, CountDownLatch latch, int cores) {
         this.results = results;
         this.threadIdx = threadIdx;
         this.bind = bind;
         this.latch = latch;
+        this.cores = cores;
     }
 
     @Override
     public void run() {
+
+        BitSet bitSet = new BitSet(cores);
+        bitSet.set(threadIdx);
+        Affinity.setAffinity(bitSet);
         compute();
     }
 
