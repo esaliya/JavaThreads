2c8359874507da94ab945fd58614d814375fe3ee
esaliya <esaliya@gmail.com>
2015-11-08 01:59:51 -0500

    adding long running java threads

--- src/main/java/org/saliya/javathreads/BCReplica.java
+++ src/main/java/org/saliya/javathreads/BCReplica.java
@@ -10,6 +10,7 @@ import java.nio.DoubleBuffer;
 import java.util.BitSet;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.function.DoubleBinaryOperator;
 import java.util.stream.IntStream;
 
@@ -37,8 +38,10 @@ public class BCReplica {
         boolean useHJ = Boolean.parseBoolean(args[3]);
         int iterations = Integer.parseInt(args[4]);
 
-        final int globalColCount = args.length > 5 ? Integer.parseInt(args[5]) : 50000;
-        final int totalComputingUnits = args.length > 6 ? Integer.parseInt(args[6]) : 24 * 20;
+        boolean lrt = args.length > 5 && Boolean.parseBoolean(args[5]);
+
+        final int globalColCount = args.length > 6 ? Integer.parseInt(args[7]) : 50000;
+        final int totalComputingUnits = args.length > 7 ? Integer.parseInt(args[7]) : 24 * 20;
 
         int rowCountPerUnit = globalColCount/ totalComputingUnits;
 
@@ -46,6 +49,44 @@ public class BCReplica {
         threadPartialOutMM = new double[threadCount][rowCountPerUnit][targetDimension];
         threadPartialBofZ = new double[threadCount][rowCountPerUnit][globalColCount];
         timers = new Stopwatch[threadCount];
+
+        final CountDownLatch[] lrtLatch = new CountDownLatch[1];
+        lrtLatch[0] = new CountDownLatch(threadCount);
+        AtomicBoolean workToDo = new AtomicBoolean(false);
+        AtomicBoolean readyToGo = new AtomicBoolean(true);
+        AtomicBoolean done = new AtomicBoolean(false);
+        if (lrt){
+            if (!useHJ){
+                for (int i = 0; i < threadCount; ++i){
+                    final int threadIdx = i;
+                    new Thread(
+                        new Runnable() {
+                            @Override
+                            public void run() {
+                                while (!done.get()) {
+                                    while (!workToDo.get()) {/*busy wait*/}
+                                    if (done.get()){
+                                        lrtLatch[0].countDown();
+                                        return;
+                                    }
+
+                                    timers[threadIdx].start();
+                                    MatrixUtils.matrixMultiply(
+                                        threadPartialBofZ[threadIdx], preX,
+                                        rowCountPerUnit, targetDimension,
+                                        globalColCount, blockSize,
+                                        threadPartialOutMM[threadIdx]);
+                                    timers[threadIdx].stop();
+                                    lrtLatch[0].countDown();
+
+                                    while (!readyToGo.get()) {/*busy wait*/}
+                                }
+                            }
+                        }).start();
+                }
+            }
+        }
+
 
         DoubleBuffer buff = MPI.newDoubleBuffer(1);
         double avgTime = 0.0d;
@@ -63,51 +104,65 @@ public class BCReplica {
             IntStream.range(0,threadCount).forEach(t -> timers[t] = Stopwatch.createUnstarted());
 
             if (threadCount > 1){
-                if (useHJ) {
-                    launchHabaneroApp(
-                        () -> forallChunked(
-                            0, threadCount - 1, (threadIdx) -> {
-                                if (bind) {
-                                    BitSet bitSet = new BitSet(coreCount);
-                                    bitSet.set(threadIdx);
-                                    Affinity.setAffinity(bitSet);
-                                }
-                                timers[threadIdx].start();
-                                MatrixUtils.matrixMultiply(
-                                    threadPartialBofZ[threadIdx], preX,
-                                    rowCountPerUnit, targetDimension,
-                                    globalColCount, blockSize,
-                                    threadPartialOutMM[threadIdx]);
-                                timers[threadIdx].stop();
-                            }));
-                } else {
-                    final CountDownLatch latch = new CountDownLatch(threadCount);
-                    final CountDownLatch go = new CountDownLatch(1);
-                    for (int i = 0; i < threadCount; ++i){
-                        final int threadIdx = i;
-                        new Thread(
-                            new Runnable() {
-                                @Override
-                                public void run() {
-                                    try {
-                                        go.await();
-                                        timers[threadIdx].start();
-                                        MatrixUtils.matrixMultiply(
-                                            threadPartialBofZ[threadIdx], preX,
-                                            rowCountPerUnit, targetDimension,
-                                            globalColCount, blockSize,
-                                            threadPartialOutMM[threadIdx]);
-                                        timers[threadIdx].stop();
-                                        latch.countDown();
-                                    }
-                                    catch (InterruptedException e) {
-                                        e.printStackTrace();
-                                    }
-                                }
-                            }).start();
+                if (lrt){
+                    if (!useHJ){
+                        readyToGo.compareAndSet(true, false);
+                        workToDo.compareAndSet(false, true);
+                        lrtLatch[0].await();
+                        lrtLatch[0] = new CountDownLatch(threadCount);
+                        workToDo.compareAndSet(true, false);
+                        readyToGo.compareAndSet(false, true);
                     }
-                    go.countDown();
-                    latch.await();
+                } else {
+                    if (useHJ) {
+                        launchHabaneroApp(
+                            () -> forallChunked(
+                                0, threadCount - 1, (threadIdx) -> {
+                                    if (bind) {
+                                        BitSet bitSet = new BitSet(coreCount);
+                                        bitSet.set(threadIdx);
+                                        Affinity.setAffinity(bitSet);
+                                    }
+                                    timers[threadIdx].start();
+                                    MatrixUtils.matrixMultiply(
+                                        threadPartialBofZ[threadIdx], preX,
+                                        rowCountPerUnit, targetDimension,
+                                        globalColCount, blockSize,
+                                        threadPartialOutMM[threadIdx]);
+                                    timers[threadIdx].stop();
+                                }));
+                    }
+                    else {
+                        final CountDownLatch latch = new CountDownLatch(threadCount);
+                        final CountDownLatch go = new CountDownLatch(1);
+                        for (int i = 0; i < threadCount; ++i) {
+                            final int threadIdx = i;
+                            new Thread(
+                                new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        try {
+                                            go.await();
+                                            timers[threadIdx].start();
+                                            MatrixUtils.matrixMultiply(
+                                                threadPartialBofZ[threadIdx],
+                                                preX, rowCountPerUnit,
+                                                targetDimension, globalColCount,
+                                                blockSize, threadPartialOutMM
+                                                    [threadIdx]);
+                                            timers[threadIdx].stop();
+                                            latch.countDown();
+                                        }
+                                        catch (InterruptedException e) {
+                                            e.printStackTrace();
+                                        }
+                                    }
+                                }).start();
+                        }
+                        go.countDown();
+                        latch.await();
+
+                    }
                 }
             } else {
                 timers[0].start();
@@ -131,9 +186,20 @@ public class BCReplica {
         worldProcComm.reduce(buff, 1, MPI.DOUBLE, MPI.SUM, 0);
         if (worldProcRank == 0) {
             avgTime = buff.get(0);
-            System.out.println("Average Time " + (avgTime / (iterations*worldProcCount)) + " "
-                               + "ms");
+            System.out.println(
+                "Average Time " + (avgTime / (iterations * worldProcCount))
+                + " " + "ms");
         }
+
+        // Shutdown long running threads if any
+        if (lrt){
+            if (!useHJ){
+                done.compareAndSet(false, true);
+                workToDo.compareAndSet(false, true);
+                lrtLatch[0].await();
+            }
+        }
+
         MPI.Finalize();
     }
 }
