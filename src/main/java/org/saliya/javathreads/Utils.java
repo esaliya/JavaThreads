05d17a46f64ab1a15b28a03f537e7bba5df52a5a
esaliya <esaliya@gmail.com>
2015-11-09 23:32:50 -0500

    Adding affinity prints

--- src/main/java/org/saliya/javathreads/Utils.java
+++ src/main/java/org/saliya/javathreads/Utils.java
@@ -14,7 +14,7 @@ public class Utils {
     public static String getProcAffinityMask(int pid) throws IOException {
         byte[] bo = new byte[100];
         String pidString = String.valueOf(pid);
-        String[] cmd = {"bash", "-c", "taskset -p " + pidString};
+        String[] cmd = {"bash", "-c", "taskset -pc " + pidString};
         Process p = Runtime.getRuntime().exec(cmd);
         p.getInputStream().read(bo);
         return new String(bo);
