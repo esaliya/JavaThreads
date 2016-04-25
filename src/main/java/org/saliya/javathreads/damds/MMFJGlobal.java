30aafa9b3fe0dd813bc9bb64a6e1bf2e5405b8ff
Saliya Ekanayake <esaliya@gmail.com>
2016-04-25 15:00:43 -0400

    Adding MMFJGlobal

--- src/main/java/org/saliya/javathreads/damds/MMFJGlobal.java
+++ src/main/java/org/saliya/javathreads/damds/MMFJGlobal.java
@@ -1,17 +1,18 @@
 package org.saliya.javathreads.damds;
 
 import com.google.common.base.Stopwatch;
-import com.google.common.collect.Interner;
 import mpi.MPIException;
 import net.openhft.lang.io.Bytes;
 import org.saliya.javathreads.*;
 
 import java.io.IOException;
-import java.util.Random;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.IntStream;
 
-public class MMMpi {
+import static edu.rice.hj.Module0.launchHabaneroApp;
+import static edu.rice.hj.Module1.forallChunked;
+
+public class MMFJGlobal {
     static double [][][] threadPartialBofZ;
     static double [] preX;
     static double [][] threadPartialMM;
@@ -20,9 +21,10 @@ public class MMMpi {
     static Stopwatch timer;
     static Stopwatch commTimer;
     static Stopwatch compTimer;
+    static Stopwatch[] compInternalTimer;
 
-    static long accumTime = 0L, accumCompTime = 0L, accumCommTime=0L;
-    static long time = 0L, compTime = 0L, commTime=0L;
+    static long sumTime = 0L, sumCompTime = 0L, sumCommTime =0L;
+    static long time = 0L, compTime = 0L, maxCompTime = 0L, commTime=0L;
 
 
     public static void main(String[] args)
@@ -31,8 +33,10 @@ public class MMMpi {
         final int iterations = Integer.parseInt(args[0]);
         final int globalColCount = Integer.parseInt(args[1]);
         ParallelOps.nodeCount = Integer.parseInt(args[2]);
-        ParallelOps.mmapScratchDir = (args.length > 3) ? args[3] : "/dev/shm";
-        ParallelOps.threadCount = 1;
+        blockSize = (args.length > 3) ? Integer.parseInt(args[3]) : 64;
+        ParallelOps.threadCount = (args.length > 4) ? Integer.parseInt(args[4]) : 1;
+        ParallelOps.mmapScratchDir = (args.length > 5) ? args[5] : "/dev/shm";
+
         ParallelOps.mmapsPerNode = 1;
 
         /* Set up parallelism */
@@ -41,43 +45,48 @@ public class MMMpi {
 
 
         /* Allocate arrays */
-        preX = new double[globalColCount*targetDimension];
-        threadPartialMM = new double[ParallelOps.threadCount][];
-        threadPartialBofZ = new double[ParallelOps.threadCount][][];
-        int threadRowCount;
-        for (int i = 0; i < ParallelOps.threadCount; ++i){
-            threadRowCount = ParallelOps.threadRowCounts[i];
-            threadPartialBofZ[i] = new double[threadRowCount][ParallelOps.globalColCount];
-            threadPartialMM[i] = new double[threadRowCount * targetDimension];
-        }
+        allocateArrays(globalColCount);
 
+        /* To keep things simple let's take data initialization out of the loop*/
+        initializeData(globalColCount);
+
+        /* Initialize timers */
         timer = Stopwatch.createUnstarted();
         compTimer = Stopwatch.createUnstarted();
+        compInternalTimer = new Stopwatch[ParallelOps.threadCount];
+        IntStream.range(0, ParallelOps.threadCount).forEach(i -> compInternalTimer[i] = Stopwatch.createUnstarted());
         commTimer = Stopwatch.createUnstarted();
 
-
+        /* Start main loop*/
         for (int itr = 0; itr < iterations; ++itr) {
-            /* Generate initial mapping of points */
-            generateInitMapping(globalColCount, targetDimension, preX);
-
-            /* Set random numbers for BofZ*/
-            IntStream.range(0, ParallelOps.threadCount).forEach(
-                t -> IntStream.range(0, ParallelOps.threadRowCounts[t]).forEach(
-                    r -> IntStream.range(0, globalColCount).forEach(c -> {
-                        threadPartialBofZ[t][r][c] = Math.random();
-                    })));
-
             if (ParallelOps.threadCount > 1){
-                Utils.printMessage("Threads are not supported in this version!");
+                compTimer.start();
+                launchHabaneroApp(
+                        () -> forallChunked(
+                                0, ParallelOps.threadCount - 1,
+                                (threadIdx) -> {
+                                    compInternalTimer[threadIdx].start();
+                                    MatrixUtils
+                                            .matrixMultiply(threadPartialBofZ[threadIdx], preX, ParallelOps.threadRowCounts[threadIdx],
+                                                    targetDimension, globalColCount, blockSize, threadPartialMM[threadIdx]);
+                                    compInternalTimer[threadIdx].stop();
+                                }));
+                compTimer.stop();
             } else {
                 ParallelOps.worldProcsComm.barrier();
 
                 timer.start();
                 compTimer.start();
+                compInternalTimer[0].start();
                 MatrixUtils
-                    .matrixMultiply(threadPartialBofZ[0], preX, ParallelOps.threadRowCounts[0],
-                        targetDimension, globalColCount, blockSize, threadPartialMM[0]);
+                        .matrixMultiply(threadPartialBofZ[0], preX, ParallelOps.threadRowCounts[0],
+
+                                targetDimension, globalColCount, blockSize, threadPartialMM[0]);
+                compInternalTimer[0].stop();
                 compTimer.stop();
+            }
+
+            if (ParallelOps.worldProcsCount > 1){
 
                 mergePartials(threadPartialMM, ParallelOps.mmapXWriteBytes);
 
@@ -101,28 +110,57 @@ public class MMMpi {
                 extractPoints(ParallelOps.fullXBytes,
                     ParallelOps.globalColCount,
                     targetDimension, preX);
-
-                timer.stop();
-
-                time = timer.elapsed(TimeUnit.MILLISECONDS);
-                accumTime += time;
-                compTime = compTimer.elapsed(TimeUnit.MILLISECONDS);
-                accumCompTime+=compTime;
-                commTime = commTimer.elapsed(TimeUnit.MILLISECONDS);
-                accumCommTime+=commTime;
-
-                timer.reset();
-                compTimer.reset();
-                commTimer.reset();
-
-                Utils.printMessage("Iteration " + itr + " time " + time +" ms compute " + compTime + " ms comm " + commTime + " ms");
             }
+            timer.stop();
+
+            time = timer.elapsed(TimeUnit.MILLISECONDS);
+            sumTime += time;
+
+            compTime = compTimer.elapsed(TimeUnit.MILLISECONDS);
+            sumCompTime +=compTime;
+            commTime = commTimer.elapsed(TimeUnit.MILLISECONDS);
+            sumCommTime +=commTime;
+
+            timer.reset();
+            compTimer.reset();
+            IntStream.range(0, ParallelOps.threadCount).forEach(i -> compInternalTimer[i].reset());
+            commTimer.reset();
+
+            Utils.printMessage("Iteration " + itr + " time " + time +" ms compute " + compTime + " ms comm " + commTime + " ms");
         }
-        Utils.printMessage("Total time " + accumTime +" ms compute " + accumCompTime + " ms comm " + accumCommTime + " ms");
+        Utils.printMessage("Total time " + sumTime +" ms compute " +
+                sumCompTime + " ms comm " + sumCommTime + " ms");
 
 
 
         ParallelOps.tearDownParallelism();
+    }
+
+    private static void allocateArrays(int globalColCount) {
+        preX = new double[globalColCount*targetDimension];
+        threadPartialMM = new double[ParallelOps.threadCount][];
+        threadPartialBofZ = new double[ParallelOps.threadCount][][];
+        int threadRowCount;
+        for (int i = 0; i < ParallelOps.threadCount; ++i){
+            threadRowCount = ParallelOps.threadRowCounts[i];
+            threadPartialBofZ[i] = new double[threadRowCount][ParallelOps.globalColCount];
+            threadPartialMM[i] = new double[threadRowCount * targetDimension];
+        }
+    }
+
+    private static void initializeData(int globalColCount) throws MPIException {
+    /* Generate initial mapping of points */
+        generateInitMapping(globalColCount, targetDimension, preX);
+
+            /* Set numbers for BofZ*/
+        IntStream.range(0, ParallelOps.threadCount).forEach(
+                t -> IntStream.range(0, ParallelOps.threadRowCounts[t]).forEach(
+                        r -> IntStream.range(0, globalColCount).forEach(c -> {
+                            threadPartialBofZ[t][r][c] =
+                                    ((r + c) & 1) == 0 ?
+                                            (0.9999995 / 1.0000023) :
+                                            (1.0000023 / 0.9999995);
+                        })));
     }
 
     private static void generateInitMapping(
@@ -131,14 +169,12 @@ public class MMMpi {
         Bytes fullBytes = ParallelOps.fullXBytes;
         if (ParallelOps.worldProcRank == 0) {
             int pos = 0;
-            // Use Random class for generating random initial mapping solution.
-            Random rand = new Random(System.currentTimeMillis());
             for (int i = 0; i < numPoints; i++) {
                 for (int j = 0; j < targetDim; j++) {
                     fullBytes.position(pos);
-                    fullBytes.writeDouble(rand.nextBoolean()
-                        ? rand.nextDouble()
-                        : -rand.nextDouble());
+                    fullBytes.writeDouble((((i+j) & 1) == 0)
+                        ? (0.9999995 / 1.0000023) :
+                            (1.0000023 / 0.9999995));
                     pos += Double.BYTES;
                 }
             }
