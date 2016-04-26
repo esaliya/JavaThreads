80568a444ca3b402b0c3879b0af002060fd094ae
Saliya Ekanayake <esaliya@gmail.com>
2016-04-26 16:24:08 -0400

    Adding MMFJGlobal

---
+++ src/main/java/org/saliya/javathreads/damds/global/MMFJGlobalLRT.java
@@ -0,0 +1,56 @@
+package org.saliya.javathreads.damds.global;
+
+import mpi.MPIException;
+import org.saliya.javathreads.damds.*;
+
+import java.io.IOException;
+import java.util.stream.IntStream;
+
+public class MMFJGlobalLRT extends MMLRT{
+    static double [][][] threadPartialBofZ;
+    static double [] preX;
+    static double [][] threadPartialMM;
+
+    public static void main(String[] args)
+        throws MPIException, InterruptedException, IOException {
+        setup(args);
+        MMUtils.printMessage("Running in Global Data LRT Mode");
+
+        /* Allocate global arrays */
+        allocateArrays(globalColCount);
+        /* To keep things simple let's take data initialization out of the mmLoop*/
+        initializeData(globalColCount);
+
+        MMWorker[] workers = new MMWorker[ParallelOps.threadCount];
+        IntStream.range(0, ParallelOps.threadCount).forEach(i -> workers[i] =
+                new MMWorker(i, threadPartialBofZ[i], preX, threadPartialMM[i],
+                        globalColCount, targetDimension, blockSize));
+
+        ParallelOps.worldProcsComm.barrier();
+        mmLoop(workers);
+        ParallelOps.tearDownParallelism();
+    }
+
+    private static void allocateArrays(int globalColCount) {
+        preX = new double[globalColCount*targetDimension];
+        threadPartialMM = new double[ParallelOps.threadCount][];
+        threadPartialBofZ = new double[ParallelOps.threadCount][][];
+        int threadRowCount;
+        for (int i = 0; i < ParallelOps.threadCount; ++i){
+            threadRowCount = ParallelOps.threadRowCounts[i];
+            threadPartialBofZ[i] = new double[threadRowCount][ParallelOps.globalColCount];
+            threadPartialMM[i] = new double[threadRowCount * targetDimension];
+        }
+    }
+
+    private static void initializeData(int globalColCount) throws MPIException {
+        /* Generate initial mapping of points */
+        MMUtils.generatePreX(globalColCount,
+                targetDimension, preX);
+
+        /* Set numbers for BofZ*/
+        IntStream.range(0, ParallelOps.threadCount).forEach(
+                t -> MMUtils.generateBofZ(t,
+                        globalColCount, threadPartialBofZ[t]));
+    }
+}
